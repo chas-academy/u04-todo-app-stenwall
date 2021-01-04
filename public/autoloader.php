12c45d312564bac54072df51764b0746c5cbc8ec
Karin <karin.stenwall@gmail.com>
2021-01-04 10:53:57 +0100

    Add autoloader

---
+++ public/autoloader.php
@@ -0,0 +1,15 @@
+<?php
+use App\Domain\Todo;
+use App\Domain\TodoList;
+
+function autoloader($namespace)
+{
+  $firstSlash = strpos($namespace, "\\") + 1;
+  $classpath = substr($namespace, $firstSlash);
+  $classpath = str_replace("\\", "/", $classpath);
+  $classfilename = __DIR__ . "/src/" . $classpath . ".php";
+  require_once $classfilename;
+}
+spl_autoload_register("autoloader");
+
+?>
