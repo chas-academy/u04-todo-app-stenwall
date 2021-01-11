96215d4574b0673bc22172a8dea800c5b11f41af
Karin <karin.stenwall@gmail.com>
2021-01-11 22:11:38 +0100

    Add functions to todo controller

--- public/src/controllers/todo.php
+++ public/src/controllers/todo.php
@@ -1,40 +1,35 @@
 <?php
-include_once (__DIR__ . '/../models/todo.php');
+declare(strict_types=1);
+namespace App\controllers;
+include (__DIR__ . '/../models/todo.php');
+include (__DIR__ . '/../views/views_functions.php');
+use App\views;
 
-function createTodo($text, $user_id) {
-    $text = $_POST['text'];
-    $user_id = $_SESSION['user_id'];
-
-    // anropa funktioen från user.php i models
-	createTodo($user_id, $text);
-	
-    // skicka tillbaka html för att visa att det gick bra från views mappen
-    showTodoCreatedView();
+function handleCreateTodo() {
+		
+	$errors = checkForm();
+	if (!empty($errors)) {
+		views\oops($errors);
+	} else {
+		// $listId = $_POST['list_id'];
+		$todoTitle = $_POST['todo-title'];
+		$todoDesc = $_POST['description'];
+		createTodo($todoTitle, $todoDesc); 
+		views\showTodoCreated($todoTitle, $todoDesc);
+	}
 }
 
-
- if (submitForm($_POST)) {
-	 $availableString = "";
-	 if ($_POST["available"]) {
-		$availableString = "yes";
-	 } else {
-		 $availableString = "no";
-	 } 
-	echo "
-	
-	<h1>You have added the following todo:</h1>
-	   <ul>
-			<li><b>Title:</b> {$_POST["title"]}</li>
-			<li><b>Author:</b> {$_POST["author"]}</li>
-			<li><b>Number of pages:</b> {$_POST["pages"]}</li>
-			<li><b>ISBN:</b> {$_POST["isbn"]}</li>
-			<li><b>Available:</b> {$availableString}</li>
-		</ul>
-		<a href='bookform.php' class='button'>Go back</a>";
-	 updateBooks($books);
- } else {
-	 echo "<a href='bookform.php' class='button'>Go back</a>";
- }
- ?> 
-
-?>
+function checkForm()
+{
+	$errorMessages = [];
+	// if (empty(trim($_POST["list_id"]))) {
+	// 	$errorMessages[] = "You need to choose a list";
+	// }
+	if (empty(trim($_POST["todo-title"]))) {
+		$errorMessages[] = "The todo have to have a title";
+	}
+	if (empty(trim($_POST["description"]))) {
+		$errorMessages[] = "The todo have to have a description";
+	}
+	return $errorMessages;
+}
