d84f80807e24d6746416da656a141e74189e4fd6
Karin <karin.stenwall@gmail.com>
2021-01-13 22:51:26 +0100

    Cleaning up code

--- public/addlist.php
+++ public/addlist.php
@@ -2,16 +2,12 @@
 include_once (__DIR__ . '/src/views/header.php');
 include_once (__DIR__ . '/src/controllers/todolist.php');
 use App\controllers;
-?>
 
-<div class="message-container">
+echo "<div class='message-container'>";
 
-<?php
 controllers\handleCreateList();
-?>
 
-</div>
+echo "</div>";
 
-<?php
 include_once (__DIR__ . '/src/views/footer.php');
 ?>
--- public/addtodo.php
+++ public/addtodo.php
@@ -3,13 +3,12 @@ include_once (__DIR__ . '/src/views/header.php');
 include_once (__DIR__ . '/src/controllers/todo.php');
 use App\controllers;
 
-?>
-<div class="message-container">
-<?php
-controllers\handleCreateTodo();
-?>
-</div>
+echo "<div class='message-container'>";
 
-<?php
+controllers\handleCreateTodo();
+
+echo "</div>";
+
+
 include_once (__DIR__ . '/src/views/footer.php');
 ?>
