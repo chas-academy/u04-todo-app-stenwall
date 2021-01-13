851d098f822a4edc04dfd84a056c7d7626ed59fd
Karin <karin.stenwall@gmail.com>
2021-01-13 22:52:17 +0100

    Add functions for choosing list when adding todo

--- public/src/controllers/todo.php
+++ public/src/controllers/todo.php
@@ -4,6 +4,7 @@ namespace App\controllers;
 include_once (__DIR__ . '/../models/todo.php');
 include_once (__DIR__ . '/../views/views_functions.php');
 use App\views;
+use App\models;
 
 function handleCreateTodo() {
 		
@@ -11,20 +12,20 @@ function handleCreateTodo() {
 	if (!empty($errors)) {
 		views\oops($errors);
 	} else {
-		// $listId = $_POST['list_id'];
+		$listId = $_POST['list-id'];
 		$todoTitle = $_POST['todo-title'];
 		$todoDesc = $_POST['description'];
-		createTodo($todoTitle, $todoDesc); 
-		views\showTodoCreated($todoTitle, $todoDesc);
+		models\createTodo($listId, $todoTitle, $todoDesc); 
+		views\showTodoCreated($listId, $todoTitle, $todoDesc);
 	}
 }
 
 function checkForm()
 {
 	$errorMessages = [];
-	// if (empty(trim($_POST["list_id"]))) {
-	// 	$errorMessages[] = "You need to choose a list";
-	// }
+	if (empty($_POST["list-id"])) {
+		$errorMessages[] = "You need to choose a list";
+	}
 	if (empty(trim($_POST["todo-title"]))) {
 		$errorMessages[] = "The todo have to have a title";
 	}
--- public/src/models/todo.php
+++ public/src/models/todo.php
@@ -1,11 +1,12 @@
 <?php
+namespace App\models;
 require_once (__DIR__ . ' /../../db.php');
 
 // Create:
-function createTodo(string $title, string $description)
+function createTodo(string $listId, string $title, string $description)
 {
 	$pdo = connectDB();
 	$sql = "INSERT INTO todos (user_id, list_id, task_title, task_desc) VALUES (:user_id, :list_id, :task_title, :task_desc)";
 	$stmt = $pdo->prepare($sql);
-	$stmt->execute(['user_id' => 1, 'list_id' => 1, 'task_title' => $title, 'task_desc' => $description]);
+	$stmt->execute(['user_id' => 1, 'list_id' => $listId, 'task_title' => $title, 'task_desc' => $description]);
 }
