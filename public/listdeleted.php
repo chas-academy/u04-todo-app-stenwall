b0ce8c89950ddbe9afb7a5eaf892400d361661ab
Karin <karin.stenwall@gmail.com>
2021-01-18 21:42:51 +0100

    Cleaning up some code

--- public/listdeleted.php
+++ public/listdeleted.php
@@ -1,14 +1,11 @@
 <?php
 include_once (__DIR__ . '/src/views/header.php');
-include_once (__DIR__ . '/src/controllers/todo.php');
 include_once (__DIR__ . '/src/controllers/todolist.php');
 use App\controllers;
 
 echo "<div class='message-container'>";
 
 controllers\handleDeleteList();
-
-echo "Listan har raderats!";
 
 echo "</div>";
 
