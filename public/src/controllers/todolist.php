57e18c0f3864966c60834dae0bda1bf6786f6432
Karin <karin.stenwall@gmail.com>
2021-01-18 20:36:10 +0100

    Add controller function for update and edit lists

--- public/src/controllers/todolist.php
+++ public/src/controllers/todolist.php
@@ -1,14 +1,14 @@
 <?php
 declare(strict_types=1);
 namespace App\controllers;
-include (__DIR__ . '/../models/todolist.php');
-include (__DIR__ . '/../views/views_functions.php');
+include_once (__DIR__ . '/../models/todolist.php');
+include_once (__DIR__ . '/../views/views_functions.php');
 use App\views;
 use App\models;
 
 function handleCreateList() {
 		
-	$errors = checkForm();
+	$errors = checkListForm();
 	if (!empty($errors)) {
 		views\oops($errors);
 	} else {
@@ -18,12 +18,30 @@ function handleCreateList() {
 	}
 }
 
-function checkForm()
+function checkListForm()
 {
 	$errorMessages = [];
 	if (empty(trim($_POST["list-title"]))) {
 		$errorMessages[] = "The list have to have a title";
 	}
 	return $errorMessages;
+}
+
+function handleEditList() {
+	
+	$listId = $_GET['listid'];
+	$listTitle = $_GET['listtitle'];
+
+	$todos = models\getTodosByListId($listId);
+
+	views\editList($listTitle, $listId,$todos);
+}
+
+function handleUpdateList() {
+
+	$listId = $_POST['list-id'];
+	$listTitle = $_POST['list-title'];
+
+	models\updateList($listTitle, $listId);
 }
 
